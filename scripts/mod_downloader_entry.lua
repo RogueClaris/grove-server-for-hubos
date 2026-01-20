b005a000800936743196bba25ff0152764c2f8a3
Arthur Cosentino <arthurcose@gmail.com>
2026-01-19 19:17:30 -0500

    Remove autoupdating mods

--- scripts/mod_downloader_entry.lua
+++ scripts/mod_downloader_entry.lua
@@ -1,9 +1,28 @@
 local ModDownloader = require("scripts/libs/mod_downloader")
 
 local package_ids = {
+  -- bosses
+  "BattleNetwork4.FireMan",
+  "BattleNetwork4.GutsMan",
+  "BattleNetwork5.BlizzardMan",
+  -- viruses
+  "BattleNetwork3.Basher",
+  "BattleNetwork3.Ratty",
+  "BattleNetwork3.Spikey",
+  "BattleNetwork4.Gaia",
   "BattleNetwork3.Canodumb",
   "BattleNetwork6.Mettaur",
+  "BattleNetwork5.Cactikil",
+  "BattleNetwork5.Powie",
+  "BattleNetwork6.Piranha",
+  "BattleNetwork6.Gunner",
+  -- libraries
   "BattleNetwork.Assets",
+  "BattleNetwork.FallingRock",
+  "dev.konstinople.library.ai",
+  "dev.konstinople.library.iterator",
+  "BattleNetwork6.Statuses.EnemyAlert",
+  "BattleNetwork6.TileStates.Ice",
 }
 
 ModDownloader.download_once(package_ids)
